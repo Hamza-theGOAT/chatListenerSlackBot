91ae5bb9ce4ac6bf976002e7f376d720dcfaee09
Hamza-theGOAT <hamza.b0302@gmail.com>
2025-07-31 17:41:49 +0500

    Initial template for sockethandler

--- main.py
+++ main.py
@@ -4,17 +4,32 @@ import os
 from dotenv import load_dotenv
 
 
-def getCredz():
-    load_dotenv()
+load_dotenv()
+botToken = os.getenv('botToken')
+appToken = os.getenv('socketToken')
+app = App(token=botToken)
 
-    userToken = os.getenv('userToken')
-    app = App(token=userToken)
 
-    return app
+@app.event("message")
+def eventHandler(body, say, logger):
+    print("📩 Message event received!")
+
+    userID = body['event'].get('user')
+    text = body['event'].get('text', '')
+
+    if userID == os.getenv('userID') and "--del" in text.lower():
+        print("🔔 Trigger matched!")
+        say("✅ Running the function now...")
+        chatDel()
+
+
+def chatDel():
+    print("🧹 Chat delete function called.")
 
 
 def main():
-    app = getCredz()
+    handler = SocketModeHandler(app, appToken)
+    handler.start()
 
 
 if __name__ == '__main__':
